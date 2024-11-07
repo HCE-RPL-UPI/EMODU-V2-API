92c298aea810b25daa672175a80dcd77252160ca
Rony Wahyu <ronywahyuu@upi.edu>
2024-11-07 08:14:58 +0700

    chore: adding cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*' 
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   app.setGlobalPrefix('api/v2');
